3f43029d27dd08e84eebd05f172cd4b7adbbccd0
Daniele Lanzi <dnlshiva@gmail.com>
2020-11-12 20:12:31 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeIt from '@angular/common/locales/it';
+
+registerLocaleData(localeIt);
 
 import { AppComponent } from './app.component';
+import { UcfirstPipe } from './ucfirst.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UcfirstPipe
   ],
   imports: [
     BrowserModule
